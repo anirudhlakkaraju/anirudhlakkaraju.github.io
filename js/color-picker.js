3c5c4763bcdb3aace0ec44e6499ea551530391ad
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-20 19:04:01 -0500

    feat: dynamic ring fill when picked and consistency on reload

--- js/color-picker.js
+++ js/color-picker.js
@@ -19,19 +19,47 @@ const setColorPreference = () => {
 // Function to reflect the user's preference in the document
 const reflectColorPreference = () => {
     document.firstElementChild.setAttribute('data-color', color.value);
+
+    let selectedRing = document.querySelector(`.color-picker .ring[data-color="${color.value}"]`)
+    if (selectedRing) {
+        selectedRing.classList.add('selected');
+        selectedRing.classList.remove('unselected');
+    }
 };
+
+let currentColor = null;
 
 // Ensure the DOM is fully loaded before adding event listeners
 document.addEventListener('DOMContentLoaded', () => {
+    // Set preference on screen load
+    reflectColorPreference();
+
+    // Set to color selected color on load
+    currentColor = document.querySelector(`.color-picker .ring.selected`);
+
     // Add click event listeners to each color ring
     document.querySelectorAll('.color-picker .ring').forEach(ring => {
         ring.addEventListener('click', function() {
             const colorPicked = this.getAttribute('data-color');
+
+            // If color currently selected
+            if (currentColor == this) return;
+
+            // If new color picked
+            if (currentColor) {
+                // Change background of currently selected ring to white
+                currentColor.classList.remove('selected');
+                currentColor.classList.add('unselected');
+            }
+
+            // Set the new color
+            currentColor = this;
+            currentColor.classList.remove('unselected');
+            currentColor.classList.add('selected');
+
             color.value = colorPicked;
             setColorPreference();
         });
     });
 
-    // Set preference on screen load
-    reflectColorPreference();
 });
